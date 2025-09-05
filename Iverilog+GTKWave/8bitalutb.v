1ee5d349456381f608e2190018a8607e3fe5dd43
tarun-svs88 <tarunkumarsekharamahanthi@gmail.com>
2025-09-05 16:24:28 +0530

    tb

--- Iverilog+GTKWave/8bitalutb.v
+++ Iverilog+GTKWave/8bitalutb.v
@@ -1,4 +1,6 @@
-module 8bitalu_tb;
+`timescale 1ns/1ps  
+
+module 8bitalutb;
 
 reg [7:0]A,B;
 reg [2:0] s;
@@ -10,17 +12,19 @@ wire [7:0]y;
     .B(B),
     .s(s),
     .carry(carry)
+    .y(y
+)
     );
 
 
 initial begin
 
 $dumpfile("8bitaluout.vcd");
-$dumpvars(0,8bitalu_tb);
+$dumpvars(0,8bitalutb);
 
 $monitor("Time=%0t, A=%0d, B=%0d, s=%b, y=%0d, carry=%b", $time, A, B, s, y, carry);
 
-A=8'd10 , B=8'd10,
+A=8'd10 ; B=8'd10,
 #10;
 
 s = 3'b000; #10;  
